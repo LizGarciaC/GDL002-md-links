2dd7c18dda3b383578dfdcfee5e4d1b8a1f68908
LizGarciaC <lunanobel@hotmail.com>
2019-04-28 19:08:20 -0500

    Se agrega codigo para reconocer y recibir la ruta o path

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+var program = require('commander');
+program
+  .version('1.0.0')
+  .option('-v, --validate', 'Valida si el link funciona o no')
+  .option('-s, --stats', 'Muestra información estadística sobre los links')
+  //Se le enseña a la libreria command que está esperando un path o ruta
+  .arguments('<path>')
+  // Si encuentra el path entonces ejecuta la acción que se le solicita
+  .action((path)=>{
+    console.log(path);
+  })
+  .parse(process.argv);
+
+
+//program.help();
+
+
+
 module.exports = () => {
   // ...
 };
